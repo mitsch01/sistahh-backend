b1f35ca52990bd7035a4831419757e85105f35fa
mitsch01 <schwartz.miriam@gmail.com>
2024-12-02 13:31:32 +0100

    added strapi cors info

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,11 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports =
+  [
+    {
+      name: "strapi::cors",
+      config: {
+        origin: ["https://sistahh.de"],
+        methods: ["GET", "POST", "PUT", "DELETE"]
+      }
+    }
+  ],
+  ["strapi::logger", "strapi::errors", "strapi::security", "strapi::cors", "strapi::poweredBy", "strapi::query", "strapi::body", "strapi::session", "strapi::favicon", "strapi::public"]
