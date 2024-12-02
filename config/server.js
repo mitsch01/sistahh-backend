a8c298d3a2d1ea951ad15b5fd55517e2f3929074
mitsch01 <schwartz.miriam@gmail.com>
2024-12-02 12:33:59 +0100

    updated .env

--- config/server.js
+++ config/server.js
@@ -2,9 +2,10 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
-    keys: env.array("APP_KEYS")
+    keys: env.array("APP_KEYS", ["rNpX0tjCL5d7k3VzjIELEA==", "wzLBtiOz6i+5ciahRnVRiQ=="])
   },
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false)
-  }
+  },
+  url: env("RENDER_EXTERNAL_URL")
 })
